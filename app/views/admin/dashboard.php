06a2bc1088621aab6437f5154ffcfe0598797034
Aki Reiss <joshcstnd@gmail.com>
2023-06-19 18:18:42 +0700

    lllll

---
+++ app/views/admin/dashboard.php
@@ -0,0 +1,22 @@
+<?php
+include '../layouts/header.php';
+?>
+
+
+<?php
+include '../layouts/dashboard/navbar.php';
+?>
+
+<?php
+include '../layouts/dashboard/sidebar.php';
+?>
+
+
+<main id="main" class="main">
+cvxjxvj
+</main>
+
+<?php
+
+include '../layouts/footer.php';
+?>
---
+++ app/views/layouts/dashboard/sidebar.php
@@ -0,0 +1,95 @@
+<aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+  <li class="nav-item">
+    <a class="nav-link " href="index.html">
+      <i class="bi bi-grid" style="color: white;"></i>
+      <span>Dashboard</span>
+    </a>
+  </li><!-- End Dashboard  Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-people" style="color: white;"></i><span>Cocoon Producers</span></i>
+    </a>
+    <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <!-- <li>
+        <a href="cocoonproducers.html">
+          <i class="bi bi-circle"></i><span>Add Rearer</span>
+        </a>
+      </li> -->
+      
+</ul>
+  </li>
+
+<!--Project Site-->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-register.html">
+      <i class="bi bi-house" style="color: white;"></i>
+      <span>Project Site</span>
+    </a>
+    
+  </li><!-- Project Site Nav -->
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-gear" style="color: white;"></i><span>Settings</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="profile.html">
+            <i class="bi bi-circle" ></i><span>Profile</span>
+          </a>
+        </li>
+        <li>
+          <a href="charts-apexcharts.html">
+            <i class="bi bi-circle"></i><span>Back up Data</span>
+          </a>
+        </li>
+</ul>
+   
+  </li><!-- End Settings Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#forms-nav1" data-bs-toggle="collapse" href="#">
+      <i class="bx bxs-report" style="color: white;"></i><span>Reports</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="forms-nav1" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="profile.html">
+          <i class="bi bi-circle"></i><span>List of Cocoon Producers</span>
+        </a>
+      </li>
+      <li>
+        <a href="charts-apexcharts.html">
+          <i class="bi bi-circle"></i><span>List of Project Site</span>
+        </a>
+      </li>
+      <ul id="forms-nav1" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="charts-chartjs.html">
+            <i class="bi bi-circle"></i><span>Production</span>
+          </a>
+        </li>
+        <li>
+          <a href="charts-apexcharts.html">
+            <i class="bi bi-circle"></i><span>Print/Download</span>
+          </a>
+        </li>
+</ul>
+</ul>
+  
+</.li><!-- End Reports Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-blank.html">
+      <i class="bi bi-box-arrow-in-right" style="color: white;"></i>
+      <span>Log out</span>
+    </a>
+  </li><!-- End Blank Page Nav -->
+
+</ul>
+
+</aside>
